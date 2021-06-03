dd2e7bffac8e0c9c994e2b411a06488543cc2ec2
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-06-03 11:10:55 -0300

    add diretivas

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,8 @@
 </template>
 
 <script setup>
-import App from './data-bind/App.vue'
+import App from './diretivas/App.vue'
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
---
+++ src/diretivas/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Minha lista de tarefas</h1>
+        <button @click="() => showList = !showList">Ver a lista!</button>
+        <br>
+        <ul v-if="showList">
+            <li 
+                v-for="(task, index) in tasks" 
+                :key="`${task}-${index}`"
+            >
+            {{task.name}}
+            </li>
+        </ul>
+        <p v-else>Lista de tarefas escondidas</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        showList: false,
+        tasks: [
+            {
+                name: 'Fazer o curso', 
+                isDone: false
+            }
+        ]
+    })
+}
+</script>
+
+<style scoped>
+</style>
