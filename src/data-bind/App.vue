bc4bf556873d4f75520b98a9b08c72d6d8b43300
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-06-03 10:56:12 -0300

    iniciando os estudos

---
+++ src/data-bind/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 :style="{textDecoration: decoration}">Hello {{name}}!</h1>
+        <input type="text" v-model="name">
+        <br>
+
+        <a :href="link">Link do Curso!</a>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: 'Erika',
+        link: 'https://treinamento.vuejsbrasil.org',
+        decoration: 'underline'
+    })
+}
+</script>
+
+<style scoped>
+</style>
